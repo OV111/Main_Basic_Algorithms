b19b8efed2b2bed2c7cd3a4ded9a7d0d4e9f2962
Vahe <ohanyanv703@gmail.com>
2025-01-29 22:07:36 +0400

    Bubble Sort Algorithm

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,7 +2,7 @@ function bubbleSort(arr) {
     let tmp = 0;
     for(let i = 0; i < arr.length; ++i) {
         for(let j = i + 1; j < arr.length; ++j) {
-            if(arr[i] > arr[j]) {
+if(arr[i] > arr[j]) {
                 tmp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = tmp;
