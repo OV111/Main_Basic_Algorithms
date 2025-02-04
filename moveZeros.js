244cdedddcad3a75ede41192048df173feb5aab3
Vahe <ohanyanv703@gmail.com>
2025-02-04 19:20:47 +0400

    Move Zeros to end!

--- moveZeros.js
+++ moveZeros.js
@@ -13,6 +13,6 @@ console.log(moveZerosToEnd([0, 1, 0, 3, 12])); // [1,3,12,0,0]
 //Alternative Way
 function moveZerosToEnd2(nums) {
     let zeros = nums.filter(x => x === 0);
-    return [...nums.filter(y => y !== 0),...zeros];
+    return [...nums.filter(y => y !== 0), ...zeros];
 }
 console.log(moveZerosToEnd2([1,0,3,2,0,7,0,4])); // [1,3,2,7,4,0,0,0]
