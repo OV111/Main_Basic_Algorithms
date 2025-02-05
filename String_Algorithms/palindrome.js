0020835d910ff2972ff53f8c1c214eb2fcbc4f5b
Vahe <ohanyanv703@gmail.com>
2025-02-05 17:19:11 +0400

    String Algorithms!

---
+++ String_Algorithms/palindrome.js
@@ -0,0 +1,30 @@
+function isPalindrome(str) {
+    let left = 0, right = str.length - 1;
+    while(left < right) {
+        if(str[left] === str[right]) {
+            left++;
+            right--;
+        } else {
+            return false;         // Output - false
+        }
+    } 
+    return true;
+}
+console.log(isPalindrome("racecar")); // Output - true
+
+// Another Way!
+function isPalindrome2(str){
+   let result = str.split("").reverse().join("");
+   return str === result;
+}
+console.log(isPalindrome2("racecar")); // Output - true
+
+function isPalindromeRecurcisve(str,left = 0,right = str.length-1) {
+    if(left >= right) {
+        return true;
+    } else if(str[left] !== str[right]) {
+        return false;
+    }
+    return isPalindromeRecurcisve(str,left += 1,right -= 1);
+}
+console.log(isPalindromeRecurcisve("racecar")); // Output - true!
