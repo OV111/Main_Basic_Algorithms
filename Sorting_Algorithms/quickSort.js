1b790b8cb610c884d44cca3ec2dd8133a0fa3e11
Vahe <ohanyanv703@gmail.com>
2025-02-05 17:20:12 +0400

    Sorting Algorithms!

---
+++ Sorting_Algorithms/quickSort.js
@@ -0,0 +1,21 @@
+function quickSort(arr) {
+    if(arr.length <= 1) {
+        return arr;
+    }
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+
+    for(let i = 1; i < arr.length; ++i) {
+        if(arr[i] > pivot) {
+            right.push(arr[i]);
+        } 
+        if(arr[i] < pivot) {
+            left.push(arr[i]);
+        }
+    }
+    
+    return [...quickSort(left),pivot,...quickSort(right)];
+}
+let array = [10,4,5,2,7,13,3,35,9,8,0];
+console.log(quickSort(array));
