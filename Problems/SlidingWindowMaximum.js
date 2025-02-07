b00eb5be88fb4d94049d96f4dc2809120c16fe2a
Vahe <ohanyanv703@gmail.com>
2025-02-07 17:18:45 +0400

    Sliding Window Maximum!(changable!)

--- Problems/SlidingWindowMaximum.js
+++ Problems/SlidingWindowMaximum.js
@@ -1,17 +1,17 @@
 function slidingWinMax(nums,k){
     let result = [];
-    for(let i = 0; i <= nums.length - k; i++) { // is special i <= nums.length-k ;
+    for(let i = 0; i <= nums.length - k; ++i) { // is special i <= nums.length-k;
         let subArr = nums.slice(i,i+k);
-        console.log(subArr)
-        let maxVal = Math.max(...subArr)
+        let maxVal = Math.max(...subArr);   // result.push(Math.max(...nums.slice(i,i+k)));
         result.push(maxVal);
     }
     return result;
 }
-console.log(slidingWinMax([1,4,8,2,9,0,1,3,4],2)); // [ 8, 9, 4 ]
-let nums = [1,4,8,2,9,0,1,3,4,5]
-console.log(nums.length)
+console.log(slidingWinMax([1,4,8,2,9,0,1,3,4],3)); // [8, 8, 9, 9, 9, 3, 4]
+//Alternative Way! Two-Pass Left-Right Max Approach
+function slidingWinMax2(arr,k) {
 
+}
+console.log(slidingWinMax2([3, 1, 4, 1, 5, 9, 2, 6],3)); // 
 
-//Alternative Way!
-//More Advanced with deque!
+//More Advanced with deque(double-ended queue)!
